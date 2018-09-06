d70a9036442d88e619820cffa2dfcfbd46ab2223
jhendryUK <jasonhendry1987@gmail.com>
2018-09-06 08:48:40 +0100

    creating wrapper script to manage synergys

---
+++ scripts/synergys.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+echo Killing synergys daemon
+killall synergys
+
+pid=`ps -ax -o pid,command | egrep -- 'ssh -fNR .* 10.255.253.50' | grep -v grep | awk '{print $1}' | tr '\n' ' '`
+if [ "$pid" ]; then
+    echo "Killing reverse port-forward ssh sessions ($pid)"
+    for i in $pid; do
+        kill $i
+    done
+fi
+
+set -x
+
+echo Starting Synergys
+/usr/bin/synergys -f --debug INFO --name twigz -c ~/.config/synergy/config --address 127.0.0.1:24800 &
+
+echo Starting SSH
+ssh -fNR 127.0.0.1:24800:127.0.0.1:24800 10.255.253.50
