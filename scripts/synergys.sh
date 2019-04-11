6f0867e11eb754314d4269a471649bc11ac2ad91
jhendryUK <jasonhendry1987@gmail.com>
2019-04-11 09:04:00 +0100

    minor changes to pkgs and synergy script

--- scripts/synergys.sh
+++ scripts/synergys.sh
@@ -4,7 +4,10 @@
 echo Killing synergys daemon
 killall synergys
 
-pid=`ps -ax -o pid,command | egrep -- 'ssh -fNR .* 10.255.253.50' | grep -v grep | awk '{print $1}' | tr '\n' ' '`
+CLIENT=10.255.253.50
+CLIENT=10.255.252.52
+
+pid=`ps -ax -o pid,command | egrep -- "ssh -fNR .* $CLIENT" | grep -v grep | awk '{print $1}' | tr '\n' ' '`
 if [ "$pid" ]; then
     echo "Killing reverse port-forward ssh sessions ( $pid)"
     for i in $pid; do
@@ -18,4 +21,4 @@ echo Starting Synergys
 /usr/bin/synergys -f --debug INFO --name twigz -c ~/.config/synergy/config --address 127.0.0.1:24800 &
 
 echo Starting SSH
-ssh -fNR 127.0.0.1:24800:127.0.0.1:24800 10.255.253.50
+ssh -fNR 127.0.0.1:24800:127.0.0.1:24800 $CLIENT
